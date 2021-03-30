7ab9cc5567c8047be4cdbd396cab7d57855db8e1
elisabeth <elisabethea2@uoc.edu>
2021-03-30 18:01:23 +0200

    Standardize & change feature tags

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ driver = webdriver.Chrome('chromedriver')
 driver.get(url)
 
 time.sleep(2)  # Allow 2 seconds for the web page to open
-scroll_pause_time = 1 # You can set your own pause time. My laptop is a bit slow so I use 1 sec
-screen_height = driver.execute_script("return window.screen.height;")   # get the screen height of the web
+scroll_pause_time = 1
+screen_height = driver.execute_script("return window.screen.height;")   # get screen height
 i = 1
 while True:
-    # scroll one screen height each time
+    # scroll screen height
     driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))
     i += 1
     time.sleep(scroll_pause_time)
@@ -29,15 +29,14 @@ while True:
 
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-
 listings = soup.find_all('div', 'posr h100p w100p')
 
-RULES_SEARCH_PAGE = {
+FEATURES = {
     'name': {'tag': 'div', 'class': 'text-medium fwb c-black lh18 ovh text-overflow'},
     'price': {'tag': 'span', 'class': 'fwb wsnw fz16'},
     'location': {'tag': 'span', 'class': 'c-gray-extra-dark text-small text-overflow'},
-    'rating': {'tag': 'div', 'class': 'df aib cols>m4 text-medium c-accent-normal'},
-    'cancelation': {'tag': 'span', 'class': 'fz11'},
+    'rating': {'tag': 'span', 'class': 'text-small c-accent-normal'},
+    'important_info': {'tag': 'span', 'class': 'fz11'},
     'specifications': {'tag': 'div', 'class': 'text-small text-overflow'},
     'link': {'tag': 'a', 'get': 'href'}
 }
@@ -62,11 +61,11 @@ def extract_element(listing_html, params):
 
 one_row = []
 for i in range(len(listings)):
-    for feature in RULES_SEARCH_PAGE:
+    for feature in FEATURES:
         try:
-            one_row.append(extract_element(listings[i], RULES_SEARCH_PAGE[feature]))
+            one_row.append(extract_element(listings[i], FEATURES[feature]))
         except:
-            one_row.append("empty")
+            one_row.append('empty')
 
 list_of_rows = [one_row[i:i+7] for i in range(0, len(one_row), 7)]
 
@@ -75,5 +74,5 @@ with open('hometogo.csv', 'w', newline='') as f:
     writer.writerows(list_of_rows)
     
 df = pd.read_csv('hometogo.csv')
-df.columns = list(RULES_SEARCH_PAGE.keys())
+df.columns = list(FEATURES.keys())
 df.to_csv('hometogo.csv')
